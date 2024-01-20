5e1854f9df140eed3c0c1009e9cbb93e13adef11
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2024-01-20 12:18:20 -0600

    feature: add a couple ways to rotate a matrix

---
+++ rotate_matrix.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""A couple different ways to rotate a matrix 90 degrees clockwise"""
+
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def print_matrix(matrix: list[list[int]]) -> None:
+    """Print out a 2d list (matrix)"""
+    for row in matrix:
+        for value in row:
+            print(value, end=" ")
+        print()
+    print()
+
+
+def just_loops(matrix: list[list[int]]) -> list[list[int]]:
+    """
+    Other than generating the 2d new_matrix pythonically,
+    this function uses an algorithm which should be viable
+    in any procedural language.
+    """
+    new_matrix: list[list[int]] = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
+    for row_ptr, row in enumerate(matrix):
+        for col_ptr, _ in enumerate(row):
+            new_matrix[col_ptr][row_ptr] = matrix[(-row_ptr + len(row) - 1) % len(row)][col_ptr]
+    return new_matrix
+
+
+def with_zip(matrix: list[list[T]]) -> list[list[T]]:
+    """Pretty Pythonic way of rotating a list of lists"""
+    return list(zip(*matrix[::-1]))
+
+
+def main():
+    """Run all of the algoritms"""
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    print_matrix(matrix)
+    print_matrix(just_loops(matrix))
+    print_matrix(with_zip(matrix))
+
+
+if __name__ == "__main__":
+    main()
